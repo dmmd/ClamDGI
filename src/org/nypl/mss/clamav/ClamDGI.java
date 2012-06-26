3f3e94b97daddd700291d6903a11f6971c778f84
Donald Mennerich <donaldmennerich@nypl.org>
2012-06-26 10:35:40 -0400

    wrapper converted for DGI

--- src/org/nypl/mss/clamav/ClamDGI.java
+++ src/org/nypl/mss/clamav/ClamDGI.java
@@ -1,9 +1,11 @@
 package org.nypl.mss.clamav;
 
 import java.io.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 public class ClamDGI {
     public static void main(String[] args) throws FileNotFoundException, IOException{
@@ -11,9 +13,18 @@ public class ClamDGI {
         logger.setLevel(Level.OFF);
         BasicConfigurator.configure();
         
-        InputStream fis = new FileInputStream(new File("/Users/dm/Desktop/workspace/eicar.com"));
+        InputStream fis = new FileInputStream(new File(args[0]));
         ClamScan c = new ClamScan("127.0.0.1", 3310, 600);
         ScanResult result = c.scan(fis);
-        System.out.println("virus-scan: " + result.getResult());
+                Pattern pattern = Pattern.compile("FOUND");
+        Matcher matcher = pattern.matcher(result.getResult());
+        
+        if(matcher.find()){
+            System.out.println("Virus-Found: Found");
+            System.out.println("Virus-Signature: " + result.getSignature());
+        }
+        else{
+            System.out.println("Virus-Found: Not Found");
+        }
     }
 }
